d57c79a2486450fa97393686414cd43d8fc36cf2
Fredrik H. M. Saether <fredrikhms@gmail.com>
2024-07-10 12:00:49 +0000

    Enabled flakes

--- wsl-nixos/flake.nix
+++ wsl-nixos/flake.nix
@@ -1,21 +1,35 @@
 # https://github.com/nix-community/NixOS-WSL/blob/main/docs/src/how-to/nix-flakes.md
+# https://github.com/Misterio77/nix-starter-configs/blob/main/minimal/flake.nix
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+  outputs = {
+    self, 
+	nixpkgs, 
+	nixos-wsl, 
+	... 
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+	# NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
+      # FIXME replace with your hostname (current "nixos")
       nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
         system = "x86_64-linux";
+        # > Our main nixos configuration file <
         modules = [
-          nixos-wsl.nixosModules.default
+		  nixos-wsl.nixosModules.default
           {
-            system.stateVersion = "24.05";
+		    nix.settings.experimental-features = "nix-command flakes";
             wsl.enable = true;
+			system.stateVersion = "23.05";
           }
-        ];
+        ] ;
       };
     };
   };
