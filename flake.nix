db5baae2c6d44dad87c2fc24adbbe2cfeca7a172
Fredrik H. M. Sæther <fhsaethe@uio.no>
2024-08-20 12:29:56 +0200

    revert to using fish

--- flake.nix
+++ flake.nix
@@ -8,12 +8,12 @@
     {
       packages.x86_64-linux.default = pkgs.mkShell {
         packages =  [
-		  #Shells
-		  pkgs.fish
-          pkgs.zsh
+          #Shells	 
+          pkgs.fish
+          #pkgs.zsh
           pkgs.nushell  
-		
-		  #Shell setup
+
+          #Shell setup
           #pkgs.direnv pkgs.nix-direnv # https://tonyfinn.com/blog/nix-from-first-principles-flake-edition/nix-8-flakes-and-developer-environments/
 		  pkgs.any-nix-shell 
           pkgs.starship # Nicer shell env
@@ -29,13 +29,13 @@
           pkgs.yq   	# Json query
 		  pkgs.jq		# YAML query
 		  pkgs.jid  
-          pkgs.dos2unix 
+      pkgs.dos2unix 
 		  pkgs.devenv
 		  
-		  # Fun 
-		  pkgs.newsboat # RSS reader
-		  
-		  #OLD
+          # Fun 
+          pkgs.newsboat # RSS reader
+		       
+          #OLD
           # pkgs.nixFlakes # Flakes (remember to update ~/.config/nix/nix.conf) # https://yuanwang.ca/posts/getting-started-with-flakes.html
           # jetbrains.idea-community # idea-community 
         ];
@@ -43,28 +43,24 @@
 	
         # Note that `shellHook` still uses bash syntax.
         # This starts zsh, then exists the bash shell when fish exits.
-		shellHook = ''
-          zsh && exit
+        #shellHook = ''
+        #   zsh && exit
 		#  
 		#	any-nix-shell zsh --info-right | source /dev/stdin
-		#	eval "$(starship init zsh)"
+    #	eval "$(starship init zsh)"
 		#   eval "$(zoxide init zsh)"
 		#  
 		#  " 
+    #    '';
+		
+		
+        shellHook = ''
+          fish --init-command "
+            any-nix-shell fish --info-right | source
+            starship init fish | source
+            #zoxide init fish | source
+          " && exit
         '';
-		
-		
-        # Note that `shellHook` still uses bash syntax.
-        # This starts fish, then exists the bash shell when fish exits.
-        #shellHook = ''
-        #  fish --init-command "
-		#  
-		#	any-nix-shell fish --info-right | source
-		#	starship init fish | source
-		#	zoxide init fish | source
-		#  
-		#  " && exit
-        #'';
       };
     };
 }
