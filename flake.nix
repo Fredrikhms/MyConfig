dbaad9b93cffe3f9df99661ef8222751528b32aa
Fredrik H. M. Sæther <fhsaethe@uio.no>
2024-08-27 19:52:18 +0200

    added mtuxinator

--- flake.nix
+++ flake.nix
@@ -8,59 +8,64 @@
     {
       packages.x86_64-linux.default = pkgs.mkShell {
         packages =  [
-          #Shells	 
+          #Shells  
           pkgs.fish
           #pkgs.zsh
           pkgs.nushell  
 
           #Shell setup
           #pkgs.direnv pkgs.nix-direnv # https://tonyfinn.com/blog/nix-from-first-principles-flake-edition/nix-8-flakes-and-developer-environments/
-		  pkgs.any-nix-shell 
+          pkgs.any-nix-shell 
           pkgs.starship # Nicer shell env
-		  
-		  # Shell tools
-		  pkgs.tmux 	# tmux + TmuxPackagManager  https://www.youtube.com/watch?v=DzNmUNvnB04
-          pkgs.zoxide 	# smarter cd command, inspired by z and autojump (needs setup with nu)
+      
+          # Shell tools
+          pkgs.tmux   # tmux + TmuxPackagManager  https://www.youtube.com/watch?v=DzNmUNvnB04
+          pkgs.tmuxinator # Tmux session handler
+          pkgs.byobu # Tmux client wrapper?
+          pkgs.zoxide # smarter cd command, inspired by z and autojump (needs setup with nu)
           
           # Dev tools
           pkgs.git
           pkgs.helix pkgs.nano pkgs.neovim # Editor
-          pkgs.nil  	# Nix grammar
-          pkgs.yq   	# Json query
-		  pkgs.jq		# YAML query
-		  pkgs.jid  
-      pkgs.dos2unix 
-		  pkgs.devenv
-		  
+          pkgs.nil  # Nix grammar
+          pkgs.yq # Json query
+          pkgs.jq   # YAML query
+          pkgs.jid  
+          pkgs.dos2unix 
+          pkgs.devenv
+      
           # Fun 
           pkgs.newsboat # RSS reader
-		       
+           
           #OLD
           # pkgs.nixFlakes # Flakes (remember to update ~/.config/nix/nix.conf) # https://yuanwang.ca/posts/getting-started-with-flakes.html
           # jetbrains.idea-community # idea-community 
         ];
-		
-	
-        # Note that `shellHook` still uses bash syntax.
-        # This starts zsh, then exists the bash shell when fish exits.
+
+        
+        # This starts zsh.
         #shellHook = ''
         #   zsh && exit
-		#  
-		#	any-nix-shell zsh --info-right | source /dev/stdin
-    #	eval "$(starship init zsh)"
-		#   eval "$(zoxide init zsh)"
-		#  
-		#  " 
-    #    '';
-		
-		
+        #  
+        # any-nix-shell zsh --info-right | source /dev/stdin
+        # eval "$(starship init zsh)"
+        #   eval "$(zoxide init zsh)"
+        #  
+        #  " 
+        #    '';
+        
+    
         shellHook = ''
           fish --init-command "
             any-nix-shell fish --info-right | source
             starship init fish | source
             #zoxide init fish | source
+            set -gx EDITOR "hx"
+            tmux set mouse on
           " && exit
         '';
-      };
-    };
+
+     };
+  };
 }
+
