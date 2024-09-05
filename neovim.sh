3339bacb0bdf2022b62513edea38c1b73cdc387f
Fredrik H. M. Saether <fredrikhms@gmail.com>
2024-09-05 21:38:09 +0000

    Add other neovim suggestions

--- neovim.sh
+++ neovim.sh
@@ -1,2 +1,11 @@
 #  https://github.com/dc-tec/nixvim/tree/main/config
 nix run github:dc-tec/nixvim
+
+# More popular (132 stars) 
+# https://github.com/elythh/nixvim
+# nix run 'github:elythh/nixvim'
+#
+# Other from (https://nix-community.github.io/nixvim/user-guide/config-examples.html)
+# https://github.com/GaetanLepage/nix-config/tree/master/home/modules/tui/neovim (76 stars as part of a full nix configuration.nix)
+# https://github.com/redyf/Neve (129 stars - ide like)
+# https://github.com/MikaelFangel/nixvim-config (50 stars)
