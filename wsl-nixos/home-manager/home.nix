218603fd93a3d85fa74d7671c5744454095e56f5
Fredrik H. M. Saether <fredrikhms@gmail.com>
2024-09-06 15:19:56 +0000

    Atempt to setup fish with home manager

--- wsl-nixos/home-manager/home.nix
+++ wsl-nixos/home-manager/home.nix
@@ -1,11 +1,10 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
+{ inputs
+, lib
+, config
+, pkgs
+, ...
 }: {
   # You can import other home-manager modules here
   imports = [
@@ -46,16 +45,23 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  home.packages = with pkgs; [ fish ];
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = '' 
+      set fish_greeing # Disable greeting
+    '';
+
+  };
+  home.packages = with pkgs; [ fishPlugins.z ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
-    userName  = "Fredrik H. M. S.";
+    userName = "Fredrik H. M. S.";
     userEmail = "fredrikhms@gmail.com";
   };
-  
+
   #services.fail2ban.enable = true;
 
   # Nicely reload system units when changing configs
